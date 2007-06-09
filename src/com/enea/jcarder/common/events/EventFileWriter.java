b01392b3b8068d9aaa5582c5b922fc07de24cd9a
Ulrik Svensson <ulriksv@gmail.com>
2007-06-09 20:38:37 +0200

    Improve agent performance (~40%) by buffering events

--- src/com/enea/jcarder/common/events/EventFileWriter.java
+++ src/com/enea/jcarder/common/events/EventFileWriter.java
@@ -5,20 +5,21 @@ import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
-
-import net.jcip.annotations.NotThreadSafe;
+import net.jcip.annotations.ThreadSafe;
 
 import com.enea.jcarder.util.Counter;
 import com.enea.jcarder.util.Logger;
 
 import static com.enea.jcarder.common.events.EventFileReader.EVENT_LENGTH;
 
-@NotThreadSafe
+@ThreadSafe
 public final class EventFileWriter implements LockEventListenerIfc {
-    private final ByteBuffer mBuffer = ByteBuffer.allocateDirect(EVENT_LENGTH);
+    private final ByteBuffer mBuffer =
+        ByteBuffer.allocateDirect(EVENT_LENGTH * 1024);
     private final FileChannel mFileChannel;
     private final Logger mLogger = Logger.getLogger("com.enea.jcarder");
     private final Counter mWrittenLockEvents;
+    private boolean mShutdownHookExecuted = false;
 
     public EventFileWriter(File file) throws IOException {
         mLogger.info("Opening for writing: " + file.getAbsolutePath());
@@ -29,6 +30,9 @@ public final class EventFileWriter implements LockEventListenerIfc {
                                          mLogger,
                                          100000);
         writeHeader();
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            public void run() { shutdownHook(); }
+        });
     }
 
     private void writeHeader() throws IOException {
@@ -40,23 +44,41 @@ public final class EventFileWriter implements LockEventListenerIfc {
         mFileChannel.write(header);
     }
 
-    public void onLockEvent(int lockId,
-                            int lockingContextId,
-                            int lastTakenLockId,
-                            int lastTakenLockingContextId,
-                            long threadId) throws IOException {
-        mBuffer.rewind();
+    public synchronized void onLockEvent(int lockId,
+                                         int lockingContextId,
+                                         int lastTakenLockId,
+                                         int lastTakenLockingContextId,
+                                         long threadId) throws IOException {
         mBuffer.putInt(lockId);
         mBuffer.putInt(lockingContextId);
         mBuffer.putInt(lastTakenLockId);
         mBuffer.putInt(lastTakenLockingContextId);
         mBuffer.putLong(threadId);
-        mBuffer.rewind();
-        mFileChannel.write(mBuffer);
         mWrittenLockEvents.increment();
+        if (mBuffer.remaining() < EVENT_LENGTH || mShutdownHookExecuted) {
+            writeBuffer();
+        }
     }
 
-    public void close() throws IOException {
+    private void writeBuffer() throws IOException {
+        mBuffer.flip();
+        mFileChannel.write(mBuffer);
+        mBuffer.clear();
+    }
+
+    public synchronized void close() throws IOException {
+        writeBuffer();
         mFileChannel.close();
+    }
+
+    private synchronized void shutdownHook() {
+        try {
+            if (mFileChannel.isOpen()) {
+                writeBuffer();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        mShutdownHookExecuted = true;
     }
 }
