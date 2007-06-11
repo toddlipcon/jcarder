0b3c61bd610a0bd1319c2a035068c2357c7ca1c9
Joel Rosdahl <joel@rosdahl.net>
2007-06-11 21:09:55 +0200

    Handle missing parameter in DeadLock example

--- examples/DeadLock.java
+++ examples/DeadLock.java
@@ -1,11 +1,17 @@
 public class DeadLock {
     public static void main(String[] args) throws Exception {
+        int timeToSleep;
+        if (args.length > 0) {
+            timeToSleep = Integer.parseInt(args[0]);
+        } else {
+            timeToSleep = 0;
+        }
         DataModel dataModel = new DataModel();
         @SuppressWarnings("unused")
         View view1 = new View(dataModel);
         View view2 = new View(dataModel);
         dataModel.doSomeAsynchronousModelUpdate();
-        Thread.sleep(Integer.parseInt(args[0]));
+        Thread.sleep(timeToSleep);
         view2.freeze();
         view2.thaw();
         System.out.println("Main thread finished");
