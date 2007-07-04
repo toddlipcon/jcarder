3759694c81316a62318af9c7ce4bd642d4a81aac
Joel Rosdahl <joel@rosdahl.net>
2007-07-04 11:48:23 +0200

    Update copyright blurbs

--- test/com/enea/jcarder/testclasses/instrumentation/SynchronizedMethod.java
+++ test/com/enea/jcarder/testclasses/instrumentation/SynchronizedMethod.java
@@ -1,3 +1,18 @@
+/*
+ * JCarder -- cards Java programs to keep threads disentangled
+ *
+ * Copyright (C) 2006-2007 Enea AB
+ * Copyright (C) 2007 Ulrik Svensson
+ * Copyright (C) 2007 Joel Rosdahl
+ *
+ * This program is made available under the GNU GPL version 2. See the
+ * accompanying file LICENSE.txt for details.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.
+ */
+
 package com.enea.jcarder.testclasses.instrumentation;
 
 import static org.junit.Assert.assertFalse;
