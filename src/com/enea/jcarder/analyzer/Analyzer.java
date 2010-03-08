500c0d49b519317b848e0639ba604eb41254cd77
Todd Lipcon <todd@cloudera.com>
2010-03-08 12:30:23 -0800

    Modify output of dot files such that they are in the same directory as the input

--- src/com/enea/jcarder/analyzer/Analyzer.java
+++ src/com/enea/jcarder/analyzer/Analyzer.java
@@ -337,7 +337,7 @@ public final class Analyzer {
     }
 
     private void createGraphvizFile(String s, int index) throws IOException {
-        File file = new File("jcarder_result_" + index + ".dot");
+        File file = new File(mInputDirectory, "jcarder_result_" + index + ".dot");
         System.out.println("Writing Graphviz file: " + file.getAbsolutePath());
         FileWriter fw = new FileWriter(file);
         fw.write(s);
